8b0917d833300d0e3d74ad9032c1f2ebd62c7bd1
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2020-09-22 14:05:18 +0300

    ajax

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,23 +1,16 @@
 $(function() {
-  $('#hakunappi').click(function() {
-    $.getJSON('http://api.tvmaze.com/search/shows?q=' + $('#hakuteksti').val(),
-        function(data) {
-          $('ul').empty();
-          $.each(data, function(index, value) {
-            console.log(index, value);
-            let image = '';
-            if (value.show.image) {
-              image = value.show.image.medium;
-            } else {
-              image = 'http://placekitten.com/200/300';
-            }
-            const html = `<li>
+  $('#hakunappi').click(function(){
+    $.getJSON('http://api.tvmaze.com/search/shows?q=' + $('#hakuteksti').val(), function(data){
+      $('ul').empty();
+      $.each(data, function(index, value){
+        console.log(index, value);
+        const html = `<li>
                         <h2>${value.show.name}</h2>
-                        <a href="${value.show.officialSite}">Homepage</a>
-                        <img src="${image}" alt="${value.show.name}">
+                        <a href="${value.show.officialSite ? value.show.officialSite : value.show.url}" target="_blank">Homepage</a>
+                        <img src="${value.show.image ? value.show.image.medium : 'http://placekitten.com/200/300'}" alt="${value.show.name}">
                       </li>`;
-            $('ul').append($(html));
-          });
-        });
+        $('ul').append($(html));
+      });
+    });
   });
 });
